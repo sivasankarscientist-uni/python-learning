784f5896ad4a1fe021e7f5f4cace657ff12991f4
Shiva <sivasankar.scientist@gmail.com>
2026-09-13 02:29:40 +0530

    Learn Python variables and input

---
+++ lesson2.py
@@ -0,0 +1,8 @@
+morning_visits = int(input("Enter morning visits: "))
+afternoon_visits = int(input("Enter afternoon visits: "))
+
+total_visits = morning_visits + afternoon_visits
+average_visits = total_visits / 2
+
+print(f"Total visits: {total_visits}")
+print(f"Average visits: {average_visits}")
