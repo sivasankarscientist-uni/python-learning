198c603ecda91138c3546889ec7183a47fdfbeb2
Shiva <sivasankar.scientist@gmail.com>
2026-09-17 00:21:18 +0530

    Learn Python elif conditions

---
+++ lesson4.py
@@ -0,0 +1,8 @@
+visit_count = int(input("Enter today's visit count: "))
+
+if visit_count >= 12:
+    print("Excellent performance")
+elif visit_count >= 8:
+    print("Target achieved")
+else:
+    print("Target not achieved")
